0be3fb279a14f3e614d04f54074982c9f985fa86
hu <972600470@qq.com>
2018-08-20 19:04:33 +0800

    adasdsadsadsa.

--- app/src/main/java/com/example/gittest2/MainActivity.java
+++ app/src/main/java/com/example/gittest2/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.gittest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
